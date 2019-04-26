80d98ca89868b5002fe4fd35609f9a65f86b32c6
candrancs <41993012+candrancs@users.noreply.github.com>
2019-04-27 00:48:04 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//Masukkan PHPSESSID
+$PHPSESSID = 'tmumj9nuebk3cti0ibkudm9vu6';
+//Masukkan GA
+$_ga = 'GA1.2.56487920.1553252095';
+//Masukkan GID
+$_gid = 'GA1.2.187049796.1553252095';
+?>
